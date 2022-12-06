875fcedacf555a9385fc4b6d21400509ed2c5e8c
Riccardo De Agostini <me@riccar.do>
2022-12-06 13:50:01 +0100

    Download theme from upstream now that my PR has been merged

--- src/Site/Program.cs
+++ src/Site/Program.cs
@@ -40,8 +40,9 @@ return await Bootstrapper
         fileSystem.TempPath = Path.Combine(artifactsDirectory, "temp");
     })
 
-    // Download theme from temporary PR branch until https://github.com/statiqdev/CleanBlog/pull/23 is merged
-    .AddThemeFromUri(new Uri("https://github.com/rdeago/Statiq-CleanBlog/archive/refs/heads/update-dependencies.zip"))
+    // Download a specific SHA of the theme, so updates to the theme won't disrupt the site.
+    // This SHA is when https://github.com/statiqdev/CleanBlog/pull/23 was merged.
+    .AddThemeFromUri(new Uri("https://github.com/statiqdev/CleanBlog/archive/6f532dce88b46ded2cc5262a352c3b30fa293e51.zip"))
     .AddWeb()
     .RunAsync()
     .ConfigureAwait(false);
